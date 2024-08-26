712644fffc011dd9dc2c3ec179b2539a2571cf63
Nahuel Lopez <nahuel.lopez@correo.ucu.edu.uy>
2024-08-26 20:25:57 -0300

    añadiendo menu

--- src/Menu.cs
+++ src/Menu.cs
@@ -1,12 +1,35 @@
 namespace Ucu.Poo.Restaurant;
+    using System.Collections;
 
-using System.Collections;
+    /// <summary>
+    /// Representa el conjunto de platillos <see cref="Dish"/> disponibles en el
+    /// restaurante.
+    /// </summary>
+    public class Menu
+    {
+        private ArrayList dishes = new ArrayList();
 
-/// <summary>
-/// Representa el conjunto de platillos <see cref="Dish"/> disponibles en el
-/// restaurante.
-/// </summary>
-public class Menu
-{
-    private ArrayList dishes = new ArrayList();
-}
+        //dishes lista de platillos
+        public void AddDish(Dish platoAñadir)
+        {
+            dishes.Add(platoAñadir);
+        }
+
+        public void RemoveDish(Dish platoBorrado)
+        {
+            dishes.Remove(platoBorrado);
+        }
+
+        public Dish GetDishByName(string nombre)
+        {
+            foreach (Dish plato in dishes)
+            {
+                if (plato.GetName() == nombre.ToLower())
+                {
+                    return plato;
+                }
+            }
+
+            return null;
+        }
+    }
