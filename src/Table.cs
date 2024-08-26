b0e62fbb41e5c09362020377e82e3dc05fa68cd5
mathiasm2024 <mathias.mallarini@correo.ucu.edu.uy>
2024-08-26 20:20:59 -0300

    XD

--- src/Table.cs
+++ src/Table.cs
@@ -7,9 +7,29 @@ using System.Collections;
 /// </summary>
 public class Table
 {
+    private int Number;
+    private bool IsOccupied;
     private ArrayList order = new ArrayList();
     public bool HasOrders()
     {
         return this.order.Count > 0;
     }
+
+
+    public void Occupy()
+    {
+        this.IsOccupied = True;
+    }
+
+    public void Free()
+    {
+        this.IsOccupied = False;
+        this.order.Clear(); 
+    }
+
+    public void AddToOrder(Dish Plato)
+    {
+        this.order.Add(Plato);
+    }
+    
 }
